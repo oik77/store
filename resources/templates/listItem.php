cb072c924412e5a8c3847dd5163ba2a0d599d697
oik77 <xnotdead@gmail.com>
2016-02-23 18:54:05 +0300

    improve styles

--- resources/templates/listItem.php
+++ resources/templates/listItem.php
@@ -1,7 +1,12 @@
 <div class="list-item" data-id="<?php echo validateText($productId); ?>">
-    <img class="item-img" src="<?php echo validateText($imgUrl); ?>">
-    <h3 class="item-cost"><?php echo "$" . validateText($cost); ?></h3>
-    <div class="item-content">
+    <div class="item-left">
+        <img class="item-img" src="<?php echo validateText($imgUrl); ?>">
+    </div>
+    <div class="item-right">
+        <span>$</span>
+        <span class="item-cost"><?php echo validateText($cost); ?></span>
+    </div>
+    <div class="item-center">
         <h2 class="item-name"><?php echo validateText($name); ?></h2>
         <p class="item-description"><?php echo validateText($description); ?></p>
         <button class="update-btn" type="button">Update</button>
