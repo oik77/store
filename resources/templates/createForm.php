aff6ac56dc09e50e5d8ad4fda2fde51347071e40
oik77 <xnotdead@gmail.com>
2016-02-27 23:13:40 +0300

    add labels for forms inputs

--- resources/templates/createForm.php
+++ resources/templates/createForm.php
@@ -1,8 +1,8 @@
 <form id="create-form" style="display: none">
-    Product Name: <input id="create-name" type="text" name="name"><br>
-    Cost: <input id="create-cost" type="text" name="cost"><br>
-    Description: <textarea id="create-description" name="description" rows="5" cols="40"></textarea><br>
-    Image: <input id="create-img" type="url" name="img_url"><br>
+    <label for="create-name">Product Name: </label><input id="create-name" type="text" name="name"><br>
+    <label for="create-cost">Cost: </label><input id="create-cost" type="text" name="cost"><br>
+    <label for="create-description">Description: </label><textarea id="create-description" name="description" rows="5" cols="40"></textarea><br>
+    <label for="create-img">Image: </label><input id="create-img" type="url" name="img_url"><br>
     <input type="submit" name="submit" value="Create">
     <button id="cancel-create" type="button">Cancel</button>
 </form>
--- resources/templates/updateForm.php
+++ resources/templates/updateForm.php
@@ -1,8 +1,8 @@
 <form id="update-form" style="display: none">
-    Product Name: <input id="update-name" type="text" name="name"><br>
-    Cost: <input id="update-cost" type="text" name="cost"><br>
-    Description: <textarea id="update-description" name="description" rows="5" cols="40"></textarea><br>
-    Image: <input id="update-img" type="url" name="img_url"><br>
+    <label for="update-name">Product Name: </label><input id="update-name" type="text" name="name"><br>
+    <label for="update-cost">Cost: </label><input id="update-cost" type="text" name="cost"><br>
+    <label for="update-description">Description: </label><textarea id="update-description" name="description" rows="5" cols="40"></textarea><br>
+    <label for="update-img">Image: </label><input id="update-img" type="url" name="img_url"><br>
     <input type="submit" name="submit" value="Update">
     <button id="cancel-update" type="button">Cancel</button>
 </form>
